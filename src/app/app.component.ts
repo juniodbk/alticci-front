a762846a05171def23a4d1d34ed30b278ff2a556
Junio Dutra <juniodbl@desktop.localdomain>
2022-07-21 07:55:14 -0300

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'alticci';
+  value: number = 0;
+  response: number = 0;
+  errorMsg: string = "";
+
+  constructor(private http: HttpClient) { }
+
+  callws(value: string) {
+    this.http.get<number>(`http://localhost:8080/alticci/${value}`)
+      .subscribe({
+        next: (x: number) =>{
+          this.response = x;
+          this.errorMsg = "";
+        },
+        error: (err: any) => this.errorMsg = err.error,
+        complete: () => {},
+      });
+  }
 }
